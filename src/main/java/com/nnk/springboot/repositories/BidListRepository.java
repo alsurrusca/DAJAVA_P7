66272b5dc03f8203efd39ce69aca17b658380355
alsurrusca <alissia.surrusca@gmail.com>
2023-01-17 10:06:24 +0100

    Classes for BidList modified

---
+++ src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -0,0 +1,14 @@
+package com.nnk.springboot.repositories;
+
+import com.nnk.springboot.domain.BidList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface BidListRepository extends JpaRepository<BidList, Integer> {
+
+   Optional<BidList> findById(Integer integer);
+
+}
